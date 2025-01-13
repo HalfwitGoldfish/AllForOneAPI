bd4f017f515442831851174b033beaae1b34c3a8
HalfwitGoldfish <chas2687@gmail.com>
2025-01-13 14:54:36 -0800

    fixed madlib controller

--- Controllers/MadLibController.cs
+++ Controllers/MadLibController.cs
@@ -1,3 +1,4 @@
+using AllForOneAPI.Models;
 using AllForOneAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,10 +15,10 @@ namespace AllForOneAPI.Controllers
         }
 
         [HttpGet]
-        [Route("MadLib/{animal}/{name}/{food}/{toy}/{age}/{animal2}/{name2}/{food2}/{toy2}/{age2}")]
-        public string MadLib(string animal, string name, string food, string toy, int age, string animal2, string name2, string food2, string toy2, string age2)
+        [Route("MadLib")]
+        public MadLibModel MadLib(MadLibModel madLibModel)
         {
-            return _madLibServices.MadLib(animal, name, food, toy, age, animal2, name2, food2, toy2, age2);
+            return madLibModel;
         }
     }
 }
