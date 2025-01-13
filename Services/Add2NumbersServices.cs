2a979fe49a1c6eda05dc166bf05f134e8d75247b
HalfwitGoldfish <chas2687@gmail.com>
2025-01-13 15:01:39 -0800

    added validation to add2numbers

--- Services/Add2NumbersServices.cs
+++ Services/Add2NumbersServices.cs
@@ -2,9 +2,16 @@ namespace AllForOneAPI.Services
 {
     public class Add2NumbersServices
     {
-        public string Add(int num1, int num2)
+        public string Add(string num1, string num2)
         {
-            return $"The Sum of {num1} and {num2} is {num1 + num2}.";
+            int parsedNum1;
+            int parsedNum2;
+            bool numericNum1 = int.TryParse(num1, out parsedNum1);
+            bool numericNum2 = int.TryParse(num2, out parsedNum2);
+            if (numericNum1 && numericNum2)
+                return $"The Sum of {parsedNum1} and {parsedNum2} is {parsedNum1 + parsedNum2}.";
+            
+            return $"error '{num1}' and '{num2}' are not valid integers";
         }
     }
 }
