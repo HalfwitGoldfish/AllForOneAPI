afec7e0c3a6be4136a538b4d9cca402d899de619
HalfwitGoldfish <chas2687@gmail.com>
2025-01-14 14:23:35 -0800

    made reverse it num easier to read

--- Services/ReverseItNumericServices.cs
+++ Services/ReverseItNumericServices.cs
@@ -9,7 +9,7 @@ namespace AllForOneAPI.Services
             if(numeric){
                 int left = parsedNum;
                 int reverse = 0;
-                while(Convert.ToBoolean(left)){
+                while(left != 0){
                     int r = left % 10;
                     reverse = reverse * 10 + r;
                     left /= 10;
